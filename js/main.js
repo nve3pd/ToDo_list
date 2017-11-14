c3a3770e12a4fdf394e142524902cd05a15277e3
nve3pd <uchihara1229@gmail.com>
2017-11-14 10:16:56 +0900

    rename localstorage.js to main.js

--- js/main.js
+++ js/main.js
@@ -5,6 +5,15 @@ function show() {  //debug
   alert("hoge");
 };
 
+$(loaded);
+
+function loaded() {
+  show_tasks();  // taskを描画
+  $("button").click(function() {
+    save_task();
+  });
+}
+
 function escape_html(string) {
   return string.replace(/[&'`"<>]/g, function(match) {
     return {
@@ -18,14 +27,21 @@ function escape_html(string) {
   });
 }
 
-function add_localstrage() {
+function check_text(text) {
+  if (text.length !== 0 && 20 >= text.length) {
+    return true;
+  } else {
+    alert("文字数は1~20文字にしてください");
+    return false;
+  }
+}
+
+function save_task() {
   var task = document.getElementById("todo").value;  
   var date = new Date();
 
-  if (task.length != 0 && 20 >= task.length) {
+  if (check_text(task)) {
     localStorage.setItem(date, task);  // localstorageに追加
-  } else {
-    alert("文字数は1~20文字にしてください");
   }
 };
 
@@ -34,9 +50,12 @@ function show_tasks() {
   for (var i = 0; i < localStorage.length; ++i) {
     keys.push(localStorage.key(i));  // 登録されているキーをすべて取得
   }
-
+//<input type="checkbox" name="q1" value="その1">
   keys.forEach(function(key) {
     var task = escape_html(localStorage.getItem(key))  /// htmlタグがあったらescapeさせる
-    $("#todo_list").append(key + "  " + task + " <br>"); // 要素の追加
+//    $("#todo_list").append(`<input type="checkbox" name="trigger" value="${key}">` + key + "  " + task + "</br>"); // 要素の追加
+      $("#todo_list").append(function() {
+        return `<input type="checkbox" name="trigger" value="${key}">` + key + "  " + task + "</br>"
+      });
   });
 };
